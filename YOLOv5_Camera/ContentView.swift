ec594d72f211134cd436bfdcd532c14c859b693c
hayeon <haha0726@hanyang.ac.kr>
2024-08-03 01:28:57 +0900

    Initial Commit

---
+++ YOLOv5_Camera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YOLOv5_Camera
+//
+//  Created by 최하연 on 8/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YOLOv5_Camera/YOLOv5_CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  YOLOv5_CameraApp.swift
+//  YOLOv5_Camera
+//
+//  Created by 최하연 on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct YOLOv5_CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
